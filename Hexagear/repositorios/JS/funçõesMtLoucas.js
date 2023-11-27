5dbf835524c4436e3a2f7c44cacaba3480dfb7fd
goitiueno <130381632+goitiueno@users.noreply.github.com>
2023-11-26 23:08:41 -0300

    Add files via upload

---
+++ Hexagear/repositorios/JS/funçõesMtLoucas.js
@@ -0,0 +1,21 @@
+function pistao(){
+    window.location.href="../../paginasweb/pistao.html"
+}
+function biela(){
+    window.location.href="../../paginasWeb/biela.html"
+}
+function pinos(){
+    window.location.href="../../paginasWeb/pinos.html"
+}
+function valvulaAE(){
+    window.location.href="../../paginasWeb/valvulaAE.html"
+}
+function comando(){
+    window.location.href="../../paginasWeb/comando.html"
+}
+function bronzinas(){
+    window.location.href="../../paginasWeb/bronzinas.html"
+}
+function termostato(){
+    window.location.href="../../paginasWeb/termostato.html" 
+}
---
+++ Hexagear/repositorios/Plinks/fl.js
@@ -0,0 +1,12 @@
+function falcatrua(){
+    window.location.href="FT.html"
+      
+}
+function troll(){
+    let p = document.getElementById("textotroll");
+    p.append('_Sua conta foi roubadakkkk_');
+   
+}
+function perdeu(){
+    alert("Perdeu")
+}
